4cdbbf213be07dc2916647116bf8e9fb3d6c8116
vpolets <vpolets@gmail.com>
2019-06-12 16:52:38 +0300

    fix description, renamed main function and added new tests

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -4,22 +4,22 @@
  * Wilson primes satisfy the following condition:
  *
  * 1) Let Num represent a prime number.
- * 2) Then ((Num-Num)! + 1) / (Num * Num) should give a whole number.
+ * 2) Then ((Num-1)! + 1) / (Num * Num) should give a whole number.
  *
- * Implement amIWilson function:
+ * Implement isWilsonPrime function:
  *
  * Function should return true if the given number is a Wilson prime
  *
  * Examples:
- * amIWilson(5)      returns true
- * amIWilson(9)      returns false
+ * isWilsonPrime(5)      returns true
+ * isWilsonPrime(9)      returns false
  *
  * @param {number} num
  *
  * @return {boolean}
  */
-function amIWilson(num) {
+function isWilsonPrime(num) {
   // write code here
 }
 
-module.exports = amIWilson;
+module.exports = isWilsonPrime;
