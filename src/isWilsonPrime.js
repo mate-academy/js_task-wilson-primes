a5520bf2d1744f46cc54b00f2d5ea20860d82579
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-08-17 10:21:08 +0300

    solution

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -19,7 +19,40 @@
  * @return {boolean}
  */
 function isWilsonPrime(num) {
-  // write code here
+  if (parseInt(num) !== num || num <= 0) {
+    return false;
+  }
+  // checking for prime numbers
+  if (num % 2 === 0 && num > 2) {
+    return false;
+  }
+
+  for (let i = 3; i < num; i += 2) {
+    if (num % i === 0) {
+      return false;
+    };
+  }
+
+  // factorial function
+  const factorialNum = (n) => {
+    let factorial = 1;
+    if (n === 0) {
+      return 1;
+    };
+
+    for (let i = 1; i <= n; i++) {
+      factorial *= i;
+    }
+
+    return factorial;
+  };
+
+  // is Wilson prime?
+  if ((factorialNum(num - 1) + 1) % (num * num) === 0) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = isWilsonPrime;
