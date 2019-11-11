14eca02df2228b2d2df817b2ffcd9a12d21f1dfe
vadim-os <osinsky_vadim@yahoo.com>
2019-11-11 14:42:42 +0200

    solution

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -19,7 +19,16 @@
  * @return {boolean}
  */
 function isWilsonPrime(num) {
-  // write code here
+  let factorial = 1;
+
+  for (let i = 1; i <= num - 1; i++) {
+    factorial *= i;
+  }
+
+  if ((factorial + 1) % (num * num) === 0) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = isWilsonPrime;
