877f8572fe99f9f53656302d79016eb4b2dc7d2b
art1shok <kvitkaart554@gmail.com>
2019-11-11 14:03:15 +0200

    solution

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -19,7 +19,15 @@
  * @return {boolean}
  */
 function isWilsonPrime(num) {
-  // write code here
+  return (factorial(num - 1) + 1) % (num * num) === 0;
+}
+
+function factorial(number) {
+  if (number < 0) {
+    return false;
+  }
+
+  return number !== 1 ? number * factorial(number - 1) : 1;
 }
 
 module.exports = isWilsonPrime;
