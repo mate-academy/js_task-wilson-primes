6004b785dad501185df567aa1e861ce323b6b5c6
Посіпака <minion@MacBook-Air-Posipaka.local>
2020-01-20 16:40:22 +0200

    wilson

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -19,7 +19,16 @@
  * @return {boolean}
  */
 function isWilsonPrime(num) {
+  let numMinusOneFactorial = num - 1;
   // write code here
+
+  for (let i = numMinusOneFactorial - 1; i > 0; i--) {
+    numMinusOneFactorial *= i;
+  }
+
+  const result = ((numMinusOneFactorial) + 1) / (num * num);
+
+  return Number.isInteger(result);
 }
 
 module.exports = isWilsonPrime;
