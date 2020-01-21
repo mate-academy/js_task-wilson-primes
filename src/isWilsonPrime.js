516366a2735eb6f29e915be9c7e054e8d280fd66
Viktoria <valencia88vika@gmail.com>
2020-01-21 05:56:53 +0200

    wilson primes

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -19,7 +19,19 @@
  * @return {boolean}
  */
 function isWilsonPrime(num) {
-  // write code here
+  let factorial = num - 1;
+
+  for (let i = factorial; i > 1; i--) {
+    factorial = factorial * (i - 1);
+  }
+
+  const wilson = (factorial + 1) / (num * num);
+
+  if (wilson % 1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isWilsonPrime;
