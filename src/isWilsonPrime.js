8d6c351a5faeea8173750c2bf23f73ce42028e52
Oly Timoshenko <o.b.timoshenko@gmail.com>
2020-03-25 15:11:57 +0200

    solution

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -19,7 +19,26 @@
  * @return {boolean}
  */
 function isWilsonPrime(num) {
-  // write code here
-}
+  let number;
+  let factorial;
 
+  function fact(n) {
+    return n ? n * fact(n - 1) : 1;
+  }
+
+  if ((num ^ 0) !== num) {
+    return false;
+  } else if (num > 0) {
+    factorial = fact(num - 1);
+    number = (factorial + 1) / (num * num);
+  } else {
+    factorial = fact(num * (-1) - 1);
+
+    number = num % 2 === 0 ? (factorial + 1) / (num * num)
+
+      : (factorial * (-1) + 1) / (num * num);
+  }
+
+  return (number ^ 0) === number;
+}
 module.exports = isWilsonPrime;
