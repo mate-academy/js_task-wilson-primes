7f8e8127e186c59df841d367b85d3b429d8674ce
Oleksii Mykhailov <alem.coding@gmail.com>
2020-03-25 13:29:24 +0200

    Solution

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -19,7 +19,44 @@
  * @return {boolean}
  */
 function isWilsonPrime(num) {
-  // write code here
+  function isPrime(x) {
+    let divider = 2;
+
+    while (divider < x) {
+      if (x % divider === 0) {
+        return false;
+      }
+      divider++;
+    }
+
+    return true;
+  }
+
+  function factorial(i, acc = 1) {
+    if (i === 1) {
+      return acc;
+    }
+
+    return factorial(i - 1, i * acc);
+  };
+
+  if (num < 0) {
+    return false;
+  }
+
+  if (num - (Math.trunc(num)) !== 0) {
+    return false;
+  }
+
+  if (!(isPrime(num))) {
+    return false;
+  }
+
+  if ((factorial(num - 1) + 1) % (num * num) === 0) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = isWilsonPrime;
