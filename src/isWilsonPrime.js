c00cd5010e7e639891939ab35892794b9177634b
solaryasha <solaryasha@gmail.com>
2020-05-21 19:25:25 +0300

    Solution

--- src/isWilsonPrime.js
+++ src/isWilsonPrime.js
@@ -20,6 +20,45 @@
  */
 function isWilsonPrime(num) {
   // write code here
+
+  const firstCondition = isPrime(num);
+  const secondCondition = wholeNumber(num);
+
+  if (firstCondition && secondCondition) {
+    return true;
+  }
+
+  return false;
+}
+
+function isPrime(num) {
+  if (num < 0) {
+    return false;
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function factorial(num) {
+  let sum = 1;
+
+  for (let i = num; i > 0; i--) {
+    sum *= i;
+  }
+
+  return sum;
+}
+
+function wholeNumber(num) {
+  const check = (factorial(num - 1) + 1) / num / num;
+
+  return check % 1 === 0;
 }
 
 module.exports = isWilsonPrime;
